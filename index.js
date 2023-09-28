6022d8ad194b39afd9b8f584f9857ff3af6c41f9
Leon Kinyanjui Maingi <leon.kinyanjui@student.moringaschool.com>
2023-09-28 15:17:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    {name: 'bobby', hometown: 'Tampa Bay'},
+  ];
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+console.log(findMatching(drivers, 'Bobby'));
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(driver => driver.name.toLowerCase().includes(str.toLowerCase()))
+}
+
+console.log(fuzzyMatch(drivers, 'bo'));
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
